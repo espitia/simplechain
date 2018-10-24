87937b26c651b646ba14f403eec3702ce1302e88
German Espitia <ge.espitia@gmail.com>
2018-10-24 10:41:48 -0400

    fixed spacing

--- simpleChain.js
+++ simpleChain.js
@@ -25,7 +25,7 @@ class Block{
 |  ================================================*/
 
 class Blockchain{
-  constructor(){
+  	constructor() {
 		this.getBlockHeight()
 			.then(blockHeight => {
 				if (blockHeight == -1) {
@@ -33,10 +33,10 @@ class Blockchain{
 					console.log(`Genesis block has been created!`)
 				}
 			})
-  }
+	}
 
-  // Add new block
-  async addBlock(newBlock) {
+  	// Add new block
+  	async addBlock(newBlock) {
 		try {
 			// Block height
 			let blockHeight = await db.getBlockHeight();
@@ -59,7 +59,7 @@ class Blockchain{
 		} catch (error) {
 			throw new Error(error.message)
 		}
-  }
+  	}
 
 	async getBlockHeight() {
 		try {
@@ -92,56 +92,56 @@ class Blockchain{
 			}
     }
 
-		// validate block
-		validateBlock(block) {
-			//get block hash of block to validateBlock
-			let blockHash = block.hash
+	// validate block
+	validateBlock(block) {
+		//get block hash of block to validateBlock
+		let blockHash = block.hash
 
-			// remove hash to hash contents before adding hash property
-			block.hash = ''
+		// remove hash to hash contents before adding hash property
+		block.hash = ''
 
-			// generate block hash
-			let validBlockHash = SHA256(JSON.stringify(block)).toString();
+		// generate block hash
+		let validBlockHash = SHA256(JSON.stringify(block)).toString();
 
-			// Compare
-			if (blockHash === validBlockHash) {
-					block.hash = blockHash; // reset block hash
-					return true;
-				} else {
-					throw new Error('Block #'+block.height+' has an invalid hash:\n'+blockHash+'<>'+validBlockHash);
-				}
-		}
+		// Compare
+		if (blockHash === validBlockHash) {
+				block.hash = blockHash; // reset block hash
+				return true;
+			} else {
+				throw new Error('Block #'+block.height+' has an invalid hash:\n'+blockHash+'<>'+validBlockHash);
+			}
+	}
 
    	// Validate blockchain
-		async validateChain() {
-			try {
-				//get chain of blocks
-				let blockHeight = await this.getBlockHeight()
-				let promises = []
-				for (let i = 0; i <= blockHeight; i++) { promises.push(this.getBlock(i)) }
-				let chain = await Promise.all(promises)
+	async validateChain() {
+		try {
+			//get chain of blocks
+			let blockHeight = await this.getBlockHeight()
+			let promises = []
+			for (let i = 0; i <= blockHeight; i++) { promises.push(this.getBlock(i)) }
+			let chain = await Promise.all(promises)
 
-				// loop through all blocks to validate
-				for (var x = 0; x <= chain.length; x++) {
+			// loop through all blocks to validate
+			for (var x = 0; x <= chain.length; x++) {
 
-					// check if all blocks have been validated (to be able to resolve promise)
-					if (x == chain.length) return true;
+				// check if all blocks have been validated (to be able to resolve promise)
+				if (x == chain.length) return true;
 
-					// check integrity of block
-					if (!(this.validateBlock(chain[x]))) {
-						throw new Error(`block #${x} is invalid`)
-					}
-					// check that hash of block is the 'previousHash' for next block
-					if (chain[x+1]) { // make sure there is a "next block" to check its previousHash property
-						let blockHash = chain[x].hash;
-						let previousHash = chain[x+1].previousBlockHash;
-						if (blockHash!== previousHash) throw new Error(`block #${x}'s hash does not match up to block #${x+1}'s 'previousBlockHash'`)
-					}
-
+				// check integrity of block
+				if (!(this.validateBlock(chain[x]))) {
+					throw new Error(`block #${x} is invalid`)
 				}
-			} catch(error) {
-				throw new Error(error.message)
+				// check that hash of block is the 'previousHash' for next block
+				if (chain[x+1]) { // make sure there is a "next block" to check its previousHash property
+					let blockHash = chain[x].hash;
+					let previousHash = chain[x+1].previousBlockHash;
+					if (blockHash!== previousHash) throw new Error(`block #${x}'s hash does not match up to block #${x+1}'s 'previousBlockHash'`)
+				}
+
 			}
+		} catch(error) {
+			throw new Error(error.message)
+		}
     }
 }
 
